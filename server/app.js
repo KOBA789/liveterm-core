c5997d7dc912d9614fa417e0195ee14010265a61
koba789 <kobahide789@gmail.com>
2012-04-04 16:44:45 +0900

    [from now] 2012/04/04 16:44:45
    diff --git a/server/app.js b/server/app.js
    index bc2f709..027f4e3 100644
    --- a/server/app.js
    +++ b/server/app.js
    @@ -3,13 +3,55 @@ var http = require('http'),
         EventEmitter = require('events').EventEmitter;
    
     var router = new (require('router-line').Router),
    -    pty = require('pty.js'),
    -    socketio = require('socket.io');
    +    socketio = require('socket.io'),
    +    fileServer = new(require('node-static').Server)('../static');
    
    -var app = http.createServer(function (req, res) {
    -
    +var hub = new (require('../hub')),
    +    app,
    +    io,
    +    handlers = {};
    +
    +function isObject(obj) {
    +  return typeof obj === 'object' && obj !== null;
    +}
    +
    +/*
    + * Routing Table
    + */
    +
    +function Helper(req, res, params) {
    +  this.request = req;
    +  this.response = res;
    +  this.params = params;
    +  var parsedUrl = url.parse(this.request.url, true);
    +  if (isObject(parsedUrl) && isObject(parsedUrl.query)) {
    +    for (var key in parsedUrl.query) {
    +      this.params[key] = parsedUrl.query[key];
    +    }
    +  }
    +}
    +
    +app = http.createServer(function (req, res) {
    +  var reqUrl = url.parse(req.url);
    +  var target = router.router(req.method.toUpperCase(),
    +                             reqUrl.pathname);
    +  if (target === undefined) {
    +    req.on('end', function () {
    +      fileServer.serve(req, res);
    +    });
    +  } else {
    +    var handler = new Helper(req, res, target.params);
    +    target.value.call(handler);
    +  }
     });
    
    -var io = socketio.listen(app);
    +io = socketio.listen(app);
    +
    +io.sockets.on('connection', function (socket) {
    +  socket.emit('snapshot', hub.snapshot());
    +  socket.on('data', function () {
    +    socket.emit('data');
    +  });
    +});
    
     app.listen(8124);
    \ No newline at end of file
    diff --git a/server/package.json b/server/package.json
    index 0fe82d4..66f3b8e 100644
    --- a/server/package.json
    +++ b/server/package.json
    @@ -9,7 +9,8 @@
       "dependencies": {
         "pty.js": "*",
         "router-line": "*",
    -    "socket.io": "*"
    +    "socket.io": "*",
    +    "node-static": "*"
       },
       "devDependencies": {
         "should": "*",

--- server/app.js
+++ server/app.js
@@ -3,13 +3,55 @@ var http = require('http'),
     EventEmitter = require('events').EventEmitter;
 
 var router = new (require('router-line').Router),
-    pty = require('pty.js'),
-    socketio = require('socket.io');
+    socketio = require('socket.io'),
+    fileServer = new(require('node-static').Server)('../static');
 
-var app = http.createServer(function (req, res) {
-  
+var hub = new (require('../hub')),
+    app,
+    io,
+    handlers = {};
+
+function isObject(obj) {
+  return typeof obj === 'object' && obj !== null;
+}
+
+/*
+ * Routing Table
+ */
+
+function Helper(req, res, params) {
+  this.request = req;
+  this.response = res;
+  this.params = params;
+  var parsedUrl = url.parse(this.request.url, true);
+  if (isObject(parsedUrl) && isObject(parsedUrl.query)) {
+    for (var key in parsedUrl.query) {
+      this.params[key] = parsedUrl.query[key];
+    }
+  }
+}
+
+app = http.createServer(function (req, res) {
+  var reqUrl = url.parse(req.url);
+  var target = router.router(req.method.toUpperCase(),
+                             reqUrl.pathname);
+  if (target === undefined) {
+    req.on('end', function () {
+      fileServer.serve(req, res);
+    });
+  } else {
+    var handler = new Helper(req, res, target.params);
+    target.value.call(handler);
+  }
 });
 
-var io = socketio.listen(app);
+io = socketio.listen(app);
+
+io.sockets.on('connection', function (socket) {
+  socket.emit('snapshot', hub.snapshot());
+  socket.on('data', function () {
+    socket.emit('data');
+  });
+});
 
 app.listen(8124);
