42cdbfd0e66124603c8d1d9a9be52718601d46e8
koba789 <kobahide789@gmail.com>
2012-04-04 14:59:53 +0900

    [from now] 2012/04/04 14:59:53
    diff --git a/caster/index.js b/caster/index.js
    new file mode 100644
    index 0000000..909bf1b
    --- /dev/null
    +++ b/caster/index.js
    @@ -0,0 +1,25 @@
    +var tty = require('tty'),
    +    pty = require('pty.js');
    +
    +var term = pty.spawn(process.env.SHELL, [], {
    +  name: 'xterm-color',
    +  cols: 80,
    +  rows: 30,
    +  cwd: process.env.HOME
    +});
    +
    +/*
    +term.on('data', function(data) {
    +  console.log(data);
    +});
    +*/
    +
    +process.stdin.resume();
    +tty.setRawMode(true);
    +
    +term.pipe(process.stdout);
    +process.stdin.pipe(term);
    +
    +term.on('exit', function () {
    +  process.exit();
    +});
    \ No newline at end of file

---
+++ caster/index.js
@@ -0,0 +1,25 @@
+var tty = require('tty'),
+    pty = require('pty.js');
+
+var term = pty.spawn(process.env.SHELL, [], {
+  name: 'xterm-color',
+  cols: 80,
+  rows: 30,
+  cwd: process.env.HOME
+});
+
+/*
+term.on('data', function(data) {
+  console.log(data);
+});
+*/
+
+process.stdin.resume();
+tty.setRawMode(true);
+
+term.pipe(process.stdout);
+process.stdin.pipe(term);
+
+term.on('exit', function () {
+  process.exit();
+});
