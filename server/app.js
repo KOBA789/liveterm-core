3495a7fe6b5a05e7dd532bf62ac575e4e0e8c84f
koba789 <kobahide789@gmail.com>
2012-04-09 19:01:27 +0900

    minimize

--- server/app.js
+++ server/app.js
@@ -10,66 +10,12 @@ var router = new (require('router-line').Router),
 var mterm = new (require('../master_terminal'))(80, 30),
     app,
     io,
-    viewers = 0,
-    controllers = {};
-
-controllers.top = require('./controllers/top');
-controllers.channels = require('./controllers/channels');
-
-// routes
-/*
-router.GET('/', {
-  controller: controllers.top,
-  action: 'index'
-});
-*/
-router.GET('/about', {
-  controller: controllers.top,
-  action: 'about'
-});
-
-/*
-router.GET('/auth/:provider', {
-  controller: controllers.auth,
-  action: 'show'
-});
-router.GET('/auth/:provider/callback', {
-  controller: controllers.auth,
-  action: 'create'
-});
- */
-
-router.GET('/channels', {
-  controller: controllers.channels,
-  action: 'index'
-});
-router.POST('/channels', {
-  controller: controllers.channels,
-  action: 'create'
-});
-router.GET('/channels/new', {
-  controller: controllers.channels,
-  action: 'new'
-});
-router.GET('/channels/:id', {
-  controller: controllers.channels,
-  action: 'show'
-});
+    viewers = 0;
 
 app = http.createServer(function (req, res) {
-  var reqUrl = url.parse(req.url);
-  var target = router.route(req.method.toUpperCase(),
-                            reqUrl.pathname);
-  if (target === undefined) {
-    req.on('end', function () {
-      fileServer.serve(req, res);
-    });
-  } else {
-    var Controller = target.value.controller;
-    var action = target.value.action;
-    var controller = new Controller(req, res, target.params);
-    controller[action]();
-  }
+  req.on('end', function () {
+    fileServer.serve(req, res);
+  });
 });
 
 mterm.open();
